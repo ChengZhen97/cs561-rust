30664c6734ad4ea65da0539704280b66a80e86b4
ChengZhen97 <zhenc.osu@gmail.com>
2022-02-27 18:02:01 -0800

    access secure endpoints

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Now that we know we can deserialize a hard-coded JSON into a struct model,
     // let's see if we can fetch the weather from the backend.
     //
-
+    //record the username and password
+    let mut map = HashMap::new();
+    map.insert("username", "usr");
+    map.insert("password", "pass");
+    //first get authenticated
+    let clientAuth = reqwest::Client::new();
+    let res = clientAuth.post("http://localhost:3000/v1/auth")
+        .json(&map)
+        .send()
+        .await?;
+    let auth_token = res
+        .json::<model::Token>()
+        .await?;
+    println!("\nToken from backend service:\n {:?}\n", auth_token);
+    let header_value = format!("Bearer {}", auth_token.accessToken);
+    //get weather client
     let client = reqwest::Client::new();
 
-    let response = client
-        .get("https://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=b98e3f089c86867862f28236d174368a&&units=imperial")
+    let response = client.get("http://localhost:3000/v1/weather")
+        .header(AUTHORIZATION, header_value)
         .send()
         .await?;
 
